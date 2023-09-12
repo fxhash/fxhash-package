365463dc69abb3707110cb984b1455c5897db3e9
Léo Pradel <pradel.leo@gmail.com>
2023-09-12 17:18:10 +0200

    Revert "[⟠ETH] [packages/fxhash-evm-sdk] EVM SDK scaffolding (#77)"
    This reverts commit 12c97580752210f9bdd2c8c7486c3f34617af25f.

--- packages/fxhash-evm-sdk/src/types/Sandbox.ts
+++
@@ -1,9 +0,0 @@
-export type SandboxFiles = Record<string, { blob?: Blob; url: string }>
-
-export enum SandboxFileError {
-  UNKNOWN = "UNKNOWN",
-  WRONG_FORMAT = "WRONG_FORMAT",
-  NO_INDEX_HTML = "NO_INDEX_HTML",
-  NO_SNIPPET = "NO_SNIPPET",
-  FAILED_UNZIP = "FAILED_UNZIP",
-}
