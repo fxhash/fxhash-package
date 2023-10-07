01209282aae7ff81d0229abafbbfe597d7301f0b
maerzhase <markus@lili16.de>
2023-10-07 17:00:10 +0200

    [packages/fxhash-cli] expose individual bin entries for cli

---
+++ packages/fxhash-cli/src/eject.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import yargs from "yargs"
+import { hideBin } from "yargs/helpers"
+import { commandEject } from "./cmd/eject/index"
+
+export default yargs(["eject", ...hideBin(process.argv)])
+  .scriptName("fxhash-eject")
+  .usage("Usage: $0 [options]")
+  .command(commandEject)
+  .help().argv
--- packages/fxhash-cli/src/index.ts
+++ packages/fxhash-cli/src/index.ts
@@ -4,6 +4,7 @@ import yargs from "yargs"
 import { commands } from "./cmd"
 
 export default yargs(process.argv.slice(2))
+  .scriptName("fxhash")
   .usage("Usage: $0 <command> [options]")
   .command(commands)
   .demandCommand(1, "You need to provide one command")
