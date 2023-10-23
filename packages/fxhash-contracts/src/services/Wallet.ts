76a45393d41fc9121cee6b010e85eeb62eee8da6
louis <louisholley@me.com>
2023-10-23 16:51:00 +0100

    [packages/fxhash-contracts] undo formatting change

--- packages/fxhash-contracts/src/services/Wallet.ts
+++ packages/fxhash-contracts/src/services/Wallet.ts
@@ -78,10 +78,12 @@ export class TezosWalletManager {
   rpcNodes: string[]
 
   constructor() {
-    // TODO: This can be replaced by importing the { config } from
+    // TODO: This can be replaced by importing the { config } from 
     // "@fxhash/config" and rely on the process.env.FXHASH_ENV
     const networkConfig =
-      process.env.NEXT_PUBLIC_TZ_NET === "mainnet" ? prdConfig : devConfig
+      process.env.NEXT_PUBLIC_TZ_NET === "mainnet"
+        ? prdConfig
+        : devConfig
     this.rpcNodes = networkConfig.tez.apis.rpcs
     this.tezosToolkit = new TezosToolkit(this.rpcNodes[0])
     this.instanciateBeaconWallet()
