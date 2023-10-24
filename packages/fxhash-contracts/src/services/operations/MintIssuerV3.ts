24e9bf644ae8fd7889e1c3e2cf7197ee399b68ff
Louis Holley <louisholley@me.com>
2023-10-24 19:42:53 +0100

    pass only cid for onchfs codex value

--- packages/fxhash-contracts/src/services/operations/MintIssuerV3.ts
+++ packages/fxhash-contracts/src/services/operations/MintIssuerV3.ts
@@ -107,6 +107,7 @@ class TezosMintIssuerV3Operation extends TezosContractOperation<TMintIssuerV3Ope
       data: packReserveData(reserve),
     }))
 
+
     const params = {
       amount: distribution.editions!,
       enabled: !!distribution.enabled,
@@ -129,12 +130,12 @@ class TezosMintIssuerV3Operation extends TezosContractOperation<TMintIssuerV3Ope
         codex_entry: this.params.data.onChain
           ? {
               type: 2, // ONCHFS
-              value: this.params.metadata.generativeUri!,
+              value: this.params.metadata.generativeUri.split("://")[1],
             }
           : {
               type: 0, // IPFS
               value: stringToByteString(
-                this.params.metadata.generativeUri!
+                this.params.metadata.generativeUri
               ),
             },
       },
