e58aaa000951a559f446294a12a6ca4b309c811e
ciphrd <ciphrd@fxhash.xyz>
2023-10-30 16:20:50 +0100

    proper export

--- packages/graphql-client/src/index.ts
+++ packages/graphql-client/src/index.ts
@@ -1,2 +1,6 @@
+// exports gql and fragments
 export * from "@/generated/index"
+// general typings for schema model & the like (not exported by default)
+export * from "@/generated/graphql"
+// crafted queries
 export * from "@/gql/index"
