99cb4b0b8f9b1cd70e0f66b2c0f0f3cbb6d49f0b
ciphrd <ciphrd@fxhash.xyz>
2023-11-01 09:12:48 +0100

    front-end works with new curator roles

---
+++ packages/gql/src/gql/accounts.ts
@@ -0,0 +1,32 @@
+import { graphql } from ".."
+
+/**
+ * Exposes the most basic details for general purposes.
+ */
+export const Frag_AccountBaseDetails = graphql(`
+  fragment Account_BaseDetails on Account {
+    id
+    username
+    profile {
+      picture
+    }
+  }
+`)
+
+/**
+ * Generic-purpose query for getting accounts with arbitrary filters, returning
+ * the base details of such accounts.
+ */
+export const Qu_GetAccounts = graphql(`
+  query GetAccounts($where: Account_bool_exp = {}) {
+    offchain {
+      Account(where: $where) {
+        ...Account_BaseDetails
+      }
+    }
+  }
+`)
+
+export const Qu_GetCurators = graphql(`
+  query GetCurators($where: )
+`)
