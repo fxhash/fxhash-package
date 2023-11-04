8b9e9b624c46577fb6e7c9d96a86fb33dedda056
ciphrd <ciphrd@fxhash.xyz>
2023-11-04 16:36:14 +0100

    safely delete @fxhash/hasura

--- packages/fxhash-hasura/jest.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['<rootDir>/test/*.test.ts'],
-  testPathIgnorePatterns: ['/node_modules/'],
-  reporters: ['default', 'jest-junit'],
-};
