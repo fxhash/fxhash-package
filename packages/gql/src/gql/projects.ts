a10f09b492b9a96b168f124a4c31a60d73c3b8ba
ciphrd <ciphrd@fxhash.xyz>
2023-11-19 15:23:28 +0100

    e2e support of project collaborators

--- packages/gql/src/gql/projects.ts
+++ packages/gql/src/gql/projects.ts
@@ -22,24 +22,11 @@ export const Frag_ProjectBaseDetails = graphql(`
       }
     }
     author {
-      id
-      status
-      username
-      profile {
-        picture
-      }
-      wallets {
-        ...Wallet_BaseDetails
-      }
+      ...Account_BaseDetails
     }
     collaborators {
       account {
-        id
-        status
-        username
-        profile {
-          picture
-        }
+        ...Account_BaseDetails
       }
     }
     projectMedias {
@@ -131,15 +118,22 @@ export const Mu_updateProject = graphql(`
     $projectId: uuid!
     $projectData: Project_set_input
     $projectMedias: [ProjectMedia_insert_input!]!
+    $projectCollaborators: [ProjectCollaborator_insert_input!]!
   ) {
     offchain {
       delete_ProjectMedia(where: { projectId: { _eq: $projectId } }) {
+        affected_rows
+      }
+      delete_ProjectCollaborator(where: { projectId: { _eq: $projectId } }) {
         affected_rows
       }
       update_Project(where: { id: { _eq: $projectId } }, _set: $projectData) {
         affected_rows
       }
       insert_ProjectMedia(objects: $projectMedias) {
+        affected_rows
+      }
+      insert_ProjectCollaborator(objects: $projectCollaborators) {
         affected_rows
       }
     }
