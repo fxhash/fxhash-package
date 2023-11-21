730d8b2718b1cb17c025cacb8e1d3aa3e2ecf1ae
Louis Holley <louisholley@me.com>
2023-11-21 09:30:33 +0000

    merge duplicate ListToken operations

--- packages/fxhash-eth-sdk/src/services/operations/marketplace/ListToken.ts
+++ packages/fxhash-eth-sdk/src/services/operations/marketplace/ListToken.ts
@@ -1,36 +1,28 @@
 import { EthereumContractOperation } from "../contractOperation"
-import {
-  ReservoirListingParams,
-} from "@/services/reservoir/types"
-import {
-  listToken,
-} from "../Marketplace"
+import { ReservoirListingParams } from "@/services/reservoir/types"
 import { RESERVOIR_ORDERBOOK, RESERVOIR_ORDER_KIND } from "@/services/Reservoir"
-import { getAddress } from "viem"
+
+import { listToken } from "../Marketplace"
+
 
 export type TListTokenEthV1OperationParams = {
   token: string
   tokenId: string
   price: string
-  amount: number
   expiration?: string
 }
 
 /**
- * List a token using Reservoir
+ * Call the Issuer factory to create a new project
  */
 export class ListTokenEthV1Operation extends EthereumContractOperation<TListTokenEthV1OperationParams> {
   // eslint-disable-next-line @typescript-eslint/no-empty-function, @typescript-eslint/explicit-function-return-type
   async prepare() {}
   async call(): Promise<string> {
-    const hash: string = "TODO"
-
-    // Prepare listing parameters
-    const listingParams: ReservoirListingParams = [
+    const args: ReservoirListingParams = [
       {
-        token: `${getAddress(this.params.token)}:${this.params.tokenId}`,
+        token: `${this.params.token}:${this.params.tokenId}`,
         weiPrice: this.params.price,
-        quantity: this.params.amount,
         orderbook: RESERVOIR_ORDERBOOK,
         orderKind: RESERVOIR_ORDER_KIND,
         expirationTime: this.params.expiration
@@ -38,10 +30,7 @@ export class ListTokenEthV1Operation extends EthereumContractOperation<TListToke
           : undefined,
       },
     ]
-
-    // Fetch and override steps
-
-    return await listToken(listingParams, this.manager.walletClient)
+    return await listToken(args, this.manager.walletClient)
   }
 
   success(): string {
