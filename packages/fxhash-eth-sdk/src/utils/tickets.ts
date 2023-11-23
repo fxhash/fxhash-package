c22de9c8af42c9dc53f03a381b25fdfd235d290d
Florian PAUTOT <florian.pautot@gmail.com>
2023-11-23 18:29:14 +0100

    add test for ticket helpers

--- packages/fxhash-eth-sdk/src/utils/tickets.ts
+++ packages/fxhash-eth-sdk/src/utils/tickets.ts
@@ -1,38 +1,41 @@
-import { getContract } from "viem"
+import { PublicClient, WalletClient, getContract } from "viem"
 import { EthereumWalletManager, FX_TICKETS_ABI } from ".."
 
 export async function getDailyTax(
-  walletManager: EthereumWalletManager,
+  walletClient: WalletClient,
+  publicClient: PublicClient,
   ticket: `0x${string}`,
   price: bigint
 ) {
   const contract = getContract({
     address: ticket,
     abi: FX_TICKETS_ABI,
-    walletClient: walletManager.walletClient,
-    publicClient: walletManager.publicClient,
+    walletClient: walletClient,
+    publicClient: publicClient,
   })
   const dailyTax = await contract.read.getDailyTax([price])
   return dailyTax as bigint
 }
 
 export async function isForeclosed(
-  walletManager: EthereumWalletManager,
+  walletClient: WalletClient,
+  publicClient: PublicClient,
   ticket: `0x${string}`,
   tokenId: bigint
 ) {
   const contract = getContract({
     address: ticket,
     abi: FX_TICKETS_ABI,
-    walletClient: walletManager.walletClient,
-    publicClient: walletManager.publicClient,
+    walletClient: walletClient,
+    publicClient: publicClient,
   })
   const isForeclosed = await contract.read.isForeclosed([tokenId])
   return isForeclosed as boolean
 }
 
 export async function getAuctionPrice(
-  walletManager: EthereumWalletManager,
+  walletClient: WalletClient,
+  publicClient: PublicClient,
   ticket: `0x${string}`,
   price: bigint,
   foreclosureTime: bigint
@@ -40,8 +43,8 @@ export async function getAuctionPrice(
   const contract = getContract({
     address: ticket,
     abi: FX_TICKETS_ABI,
-    walletClient: walletManager.walletClient,
-    publicClient: walletManager.publicClient,
+    walletClient: walletClient,
+    publicClient: publicClient,
   })
   const auctionPrice = await contract.read.getAuctionPrice([
     price,
@@ -51,15 +54,16 @@ export async function getAuctionPrice(
 }
 
 export async function getTaxInfo(
-  walletManager: EthereumWalletManager,
+  walletClient: WalletClient,
+  publicClient: PublicClient,
   ticket: `0x${string}`,
   tokenId: bigint
 ) {
   const contract = getContract({
     address: ticket,
     abi: FX_TICKETS_ABI,
-    walletClient: walletManager.walletClient,
-    publicClient: walletManager.publicClient,
+    walletClient: walletClient,
+    publicClient: publicClient,
   })
   const taxInfo = await contract.read.taxes([tokenId])
   return {
@@ -71,17 +75,29 @@ export async function getTaxInfo(
 }
 
 export async function getMinimumClaimValueForNewPrice(
-  walletManager: EthereumWalletManager,
+  walletClient: WalletClient,
+  publicClient: PublicClient,
   ticket: `0x${string}`,
   tokenId: bigint,
   newPrice: bigint
 ) {
-  const taxInfo = await getTaxInfo(walletManager, ticket, tokenId)
-  const isTicketForeclosed = await isForeclosed(walletManager, ticket, tokenId)
-  const newDailyTax = await getDailyTax(walletManager, ticket, newPrice)
+  const taxInfo = await getTaxInfo(walletClient, publicClient, ticket, tokenId)
+  const isTicketForeclosed = await isForeclosed(
+    walletClient,
+    publicClient,
+    ticket,
+    tokenId
+  )
+  const newDailyTax = await getDailyTax(
+    walletClient,
+    publicClient,
+    ticket,
+    newPrice
+  )
   if (isTicketForeclosed) {
     const auctionPrice = await getAuctionPrice(
-      walletManager,
+      walletClient,
+      publicClient,
       ticket,
       newPrice,
       taxInfo.foreclosureTime
