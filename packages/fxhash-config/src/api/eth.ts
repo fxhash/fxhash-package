7868a97984fcf91d40f23e6577da587ab654a069
ciphrd <ciphrd@fxhash.xyz>
2023-12-01 02:03:43 +0100

    prepare eth config prod

--- packages/fxhash-config/src/api/eth.ts
+++ packages/fxhash-config/src/api/eth.ts
@@ -11,3 +11,10 @@ export const ethTestnetApis: IEthApis = {
   safe: "https://safe-transaction-goerli.safe.global",
   whitelist: "http://localhost:9999/whitelist/new",
 }
+
+export const ethMainnetApis: IEthApis = {
+  rpcs: ["https://ethereum-goerli-rpc.allthatnode.com"],
+  reservoir: "https://api.reservoir.tools/",
+  safe: "https://safe-transaction-mainnet.safe.global/",
+  whitelist: "https://api.v2.dev.fxhash-dev.xyz/whitelist/new",
+}
