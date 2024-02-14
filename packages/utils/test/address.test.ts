5eb665fa72647d90393a8a5823598e9eb8b60e42
maerzhase <markus@lili16.de>
2024-02-14 16:31:51 +0100

    [packages/utils] [packages/utils] update address utils to handle base

--- packages/utils/test/address.test.ts
+++ packages/utils/test/address.test.ts
@@ -1,12 +1,17 @@
 import {
+  mockBlockchainAddress,
   mockEthereumAddress,
   isEthereumAddressValid,
   mockTezosAddress,
   isTezosAddressValid,
   getBlockchainFromAddress,
+  isBaseAddressValid,
+  mockBaseAddress,
+  isBlockchainAddressValid,
 } from "../src/address"
 
 const ETH_ADDRESS = "0x71C7656EC7ab88b098defB751B7401B5f6d8976F"
+const BASE_ADDRESS = ETH_ADDRESS
 
 const TEZ_ADDRESS = "tz1Lc2qBKEWCBeDU8npG6zCeCqpmaegRi6Jg"
 
@@ -19,6 +24,21 @@ describe("address tests", () => {
     const mock = mockEthereumAddress()
     expect(isEthereumAddressValid(mock)).toEqual(true)
     expect(getBlockchainFromAddress(mock)).toEqual("ETHEREUM")
+    const mock2 = mockBlockchainAddress("ETHEREUM")
+    expect(isEthereumAddressValid(mock2)).toEqual(true)
+    expect(getBlockchainFromAddress(mock2)).toEqual("ETHEREUM")
+  })
+  it("validate base address", async () => {
+    expect(isBaseAddressValid(BASE_ADDRESS)).toEqual(true)
+    expect(getBlockchainFromAddress(BASE_ADDRESS)).toEqual("ETHEREUM")
+  })
+  it("validate base address mock", async () => {
+    const mock = mockBaseAddress()
+    expect(isEthereumAddressValid(mock)).toEqual(true)
+    expect(getBlockchainFromAddress(mock)).toEqual("ETHEREUM")
+    const mock2 = mockBlockchainAddress("BASE")
+    expect(isEthereumAddressValid(mock2)).toEqual(true)
+    expect(getBlockchainFromAddress(mock2)).toEqual("ETHEREUM")
   })
   it("validate tez address", async () => {
     expect(isTezosAddressValid(TEZ_ADDRESS)).toEqual(true)
@@ -28,5 +48,17 @@ describe("address tests", () => {
     const mock = mockTezosAddress()
     expect(isTezosAddressValid(mock)).toEqual(true)
     expect(getBlockchainFromAddress(mock)).toEqual("TEZOS")
+    const mock2 = mockBlockchainAddress("TEZOS")
+    expect(isTezosAddressValid(mock2)).toEqual(true)
+    expect(getBlockchainFromAddress(mock2)).toEqual("TEZOS")
+  })
+  it("validate blockchain addresses", async () => {
+    const tezMock = mockTezosAddress()
+    const ethMock = mockEthereumAddress()
+    const baseMock = mockBaseAddress()
+    expect(isBlockchainAddressValid(tezMock)).toEqual(true)
+    expect(isBlockchainAddressValid(ethMock)).toEqual(true)
+    expect(isBlockchainAddressValid(baseMock)).toEqual(true)
+    expect(isBlockchainAddressValid("xxxyyy123")).toEqual(false)
   })
 })
