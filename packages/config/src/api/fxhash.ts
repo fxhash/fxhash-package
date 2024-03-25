c19d3624b3520a204e62db89dc2249a0fc154377
Louis Holley <louisholley@me.com>
2024-03-25 16:13:42 +0000

    fallback to not copying styles for capture proxy

--- packages/config/src/api/fxhash.ts
+++ packages/config/src/api/fxhash.ts
@@ -70,7 +70,7 @@ export const fxhashDevApis: IFxhashApis = {
         "https://fzezvbp2f74yturkj4akjyrq3e0zswhb.lambda-url.us-east-1.on.aws/",
     },
     proxy: {
-      ipfs: "ipfs://QmQJs44swYWpKtR6XaAEt13iBJkQyNTCuexofYUBugTexu",
+      ipfs: "ipfs://Qme6E5z1GDqrHyPomGEw5LChxmE9pdEYGWjwxrv6ZgwZRo",
       onchfs:
         "onchfs://63d57e37ea7b6b6fba101b09758f72561205cbf49ffdf264d0de6618646a0a5a",
     },
