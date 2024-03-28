6b01587d959cb7b409b7d8978cc4deb07c6c2598
Florian PAUTOT <florian.pautot@gmail.com>
2024-03-28 11:20:55 +0100

    add auto tag & name set for splits

--- packages/config/src/api/base.ts
+++ packages/config/src/api/base.ts
@@ -2,6 +2,7 @@ export interface IBaseApis {
   rpcs: string[]
   reservoir: string
   safe: string
+  splits: string
   whitelist: string
 }
 
@@ -9,6 +10,7 @@ export const baseTestnetApis: IBaseApis = {
   rpcs: ["https://sepolia.base.org"],
   reservoir: "https://api-base-sepolia.reservoir.tools",
   safe: "https://safe-transaction-base-testnet.safe.global/",
+  splits: "https://api.splits.org",
   whitelist: "http://localhost:9999/whitelist/new",
 }
 
@@ -16,5 +18,6 @@ export const baseMainnetApis: IBaseApis = {
   rpcs: ["https://mainnet.base.org"],
   reservoir: "https://api-base.reservoir.tools",
   safe: "https://safe-transaction-base.safe.global/",
+  splits: "https://api.splits.org",
   whitelist: "https://api.v2.dev.fxhash-dev.xyz/whitelist/new",
 }
