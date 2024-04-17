ea815eca4193ca72c7b965a85562abd6c1ba0a21
Florian PAUTOT <florian.pautot@gmail.com>
2024-04-17 17:27:39 +0200

    clean console

--- packages/eth/src/services/operations/minters/MintFixedPriceEthV1.ts
+++ packages/eth/src/services/operations/minters/MintFixedPriceEthV1.ts
@@ -56,7 +56,6 @@ export class MintFixedPriceEthV1Operation extends EthereumContractOperation<TMin
       value: this.params.price,
       chain: getCurrentChain(this.chain),
     }
-    console.log("args = ", args)
     const transactionHash = await simulateAndExecuteContract(this.manager, args)
     return {
       type: TransactionType.ONCHAIN,
