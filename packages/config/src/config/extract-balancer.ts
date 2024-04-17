a1a24645cf8972df2e8ebd941b6991a89d40b9cd
Louis Holley <louisholley@me.com>
2024-04-17 15:24:43 +0100

    move aws config back to env variables

--- packages/config/src/config/extract-balancer.ts
+++ packages/config/src/config/extract-balancer.ts
@@ -1,10 +1,4 @@
 export type ExtractBalancerConfig = {
-  awsBatchJobName: string
-  awsBatchGpuJobDefArn: string
-  awsBatchGpuJobQueueArn: string
-  awsBatchGpuJobQueuePriorityArn: string
-  awsBucketId: string
-  awsRegion: "us-east-1"
   clearGPUQueue: boolean
   fetchJobStatusIntervalMs: number
   logs: boolean
@@ -12,15 +6,6 @@ export type ExtractBalancerConfig = {
 }
 
 export const extractBalancerConfigDev: ExtractBalancerConfig = {
-  awsBatchJobName: "fxh-extract-gpu",
-  awsBatchGpuJobDefArn:
-    "arn:aws:batch:us-east-1:759392670381:job-definition/fxhash-dev-extract:7",
-  awsBatchGpuJobQueueArn:
-    "arn:aws:batch:us-east-1:759392670381:job-queue/fxhash-extract",
-  awsBatchGpuJobQueuePriorityArn:
-    "arn:aws:batch:us-east-1:759392670381:job-queue/fxhash-dev-extract-priority",
-  awsBucketId: "fxh-capture",
-  awsRegion: "us-east-1",
   clearGPUQueue: false,
   fetchJobStatusIntervalMs: 5000,
   logs: false,
@@ -33,12 +18,5 @@ export const extractBalancerConfigLocal: ExtractBalancerConfig = {
 
 export const extractBalancerConfigProd: ExtractBalancerConfig = {
   ...extractBalancerConfigDev,
-  awsBatchGpuJobDefArn:
-    "arn:aws:batch:us-east-1:759392670381:job-definition/fxhash-batch-prd-fxhash-extract",
-  awsBatchGpuJobQueueArn:
-    "arn:aws:batch:us-east-1:759392670381:job-queue/fxhash-batch-prd-gpu-spot-mix",
-  awsBatchGpuJobQueuePriorityArn:
-    "arn:aws:batch:us-east-1:759392670381:job-queue/fxhash-batch-prd-gpu-spot-mix-priority",
-  awsBucketId: "fxh-prd-batch-extract-us-east-1",
   clearGPUQueue: true,
 }
