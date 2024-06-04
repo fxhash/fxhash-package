dce1135e8e4f643404d5608909d01704569b7362
maerzhase <markus@lili16.de>
2024-06-04 10:02:31 +0200

    [packages/client] add ethereum wallet manager state

--- packages/client/src/react/provider/Wallet.tsx
+++ packages/client/src/react/provider/Wallet.tsx
@@ -2,23 +2,35 @@
 // @ts-ignore - unused
 import React, { PropsWithChildren, createContext, useState } from "react"
 import { TezosWalletManager } from "@fxhash/tez"
+import { EthereumWalletManager } from "@fxhash/eth"
 
 interface WalletContext {
   tezosWalletManager: TezosWalletManager | null
-  setTezosWalletManager: (manager: TezosWalletManager) => void
+  setTezosWalletManager: (manager: TezosWalletManager | null) => void
+  ethereumWalletManager: EthereumWalletManager | null
+  setEthereumWalletManager: (manager: EthereumWalletManager | null) => void
 }
 
 export const FxhashWalletContext = createContext<WalletContext>({
   tezosWalletManager: null,
   setTezosWalletManager: () => {},
+  ethereumWalletManager: null,
+  setEthereumWalletManager: () => {},
 })
 
 export function FxhashClientProvider(props: PropsWithChildren) {
   const [tezosWalletManager, setTezosWalletManager] =
     useState<TezosWalletManager | null>(null)
+  const [ethereumWalletManager, setEthereumWalletManager] =
+    useState<EthereumWalletManager | null>(null)
   return (
     <FxhashWalletContext.Provider
-      value={{ tezosWalletManager, setTezosWalletManager }}
+      value={{
+        tezosWalletManager,
+        setTezosWalletManager,
+        ethereumWalletManager,
+        setEthereumWalletManager,
+      }}
     >
       {props.children}
     </FxhashWalletContext.Provider>
