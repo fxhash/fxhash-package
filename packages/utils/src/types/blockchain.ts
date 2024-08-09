eefdd369493400f2d8e735533397a36ae8734fa7
ciphrd <ciphrd@fxhash.xyz>
2024-08-09 16:51:47 +0200

    fix circular dependency

--- packages/utils/src/types/blockchain.ts
+++
@@ -1,5 +0,0 @@
-import { BlockchainType } from "@fxhash/shared"
-import { enumKeys } from "./_index"
-
-export type Blockchain = keyof typeof BlockchainType
-export const BlockchainTypes = enumKeys(BlockchainType)
