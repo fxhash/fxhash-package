fa3a9f4f18ce4370adae188192142350e287416e
ciphrd <ciphrd@fxhash.xyz>
2024-08-13 15:22:12 +0200

    added comments to plugnplay client interface

--- packages/client/src/plugnplay/_interfaces.ts
+++ packages/client/src/plugnplay/_interfaces.ts
@@ -50,12 +50,48 @@ export interface IClientPlugnPlay {
    */
   disconnectWallet: (network: BlockchainNetwork) => Promise<void>
 
+  /**
+   * The GraphQL wrapper instance which should be used to make authenticated
+   * queries. The GraphQL client is synced with the currently authenticated
+   * user so that queries are properly authenticated.
+   *
+   * **ℹ️ Note**: If you wish to use your own GraphQL client, you should make
+   * sure to add the necessarry http headers based on the currently
+   * authenticated user.
+   */
   gql: IGraphqlWrapper
 
-  // todo: comments
+  /**
+   * Attempt to disconnect all the wallets currently connected to your
+   * application. If a user account is authenticated, this may trigger a
+   * logout as accounts are required to have wallet accounts attached to them.
+   */
   disconnectAllWallets: () => Promise<void>
+
+  /**
+   * Initiate an OTP email verification flow. This will send an OTP to the
+   * provided email address, which can then be used to log the user in with
+   * {@link loginWeb2}
+   */
   requestEmailOTP: IWeb3AuthWalletsSource["emailRequestOTP"]
+
+  /**
+   * Login to fxhash using fxhash embedded wallet and a web2 source of login
+   * (email, google, discord, etc...). This will generate a wallet linked to the
+   * source of authentication, which can be used in the browser context.
+   */
   loginWeb2: IWeb3AuthWalletsSource["login"]
+
+  /**
+   * Logout the account currently authenticated. If the account is linked to
+   * a wallet currently connected, this wallet will be disconnected
+   * automatically.
+   */
   logoutAccount: () => Promise<void>
+
+  /**
+   * Release the Client (detaches listeners, frees allocated resources).
+   * Once called the client cannot be used anymore.
+   */
   release: () => void
 }
