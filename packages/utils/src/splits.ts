395ef48cd458c9bfd5e764669d94525cb17b330a
maerzhase <markus@lili16.de>
2024-08-19 13:33:09 +0200

    use single form state for distribution

---
+++ packages/utils/src/splits.ts
@@ -0,0 +1,18 @@
+export type TSplitsTransformer = (splits: any[], index: number) => number
+
+export const transformSplitsEqual: TSplitsTransformer = splits => {
+  return Math.floor(1000 / splits.length)
+}
+
+export const transformSplitsSum1000: TSplitsTransformer = (splits, index) => {
+  return (
+    Math.floor(1000 / splits.length) + (index < 1000 % splits.length ? 1 : 0)
+  )
+}
+
+export const transformSplitsAccessList: TSplitsTransformer = (
+  splits,
+  index
+) => {
+  return splits[index].pct
+}
