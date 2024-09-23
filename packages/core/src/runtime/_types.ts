89b9cff4bfbb7523f008b7211088eb9c7fcf06c8
maerzhase <markus@lili16.de>
2024-09-23 13:51:23 +0200

    add comments

--- packages/core/src/runtime/_types.ts
+++ packages/core/src/runtime/_types.ts
@@ -11,8 +11,17 @@ export type RuntimeConnector = () => {
   ) => void
 }
 
+/**
+ * The execution context of the runtime
+ * - standalone: The runtime is used for standalone display
+ * - minting: The runtime is used for minting
+ * - capture: The runtime is used for capture
+ */
 export type ExecutionContext = "minting" | "standalone" | "capture"
 
+/**
+ * The state of a project
+ */
 export type ProjectState = {
   cid: string
   snippetVersion: string
@@ -24,6 +33,10 @@ export type ProjectState = {
   context?: ExecutionContext
 }
 
+/**
+ * The fx(params) state of a project
+ * params state consists of the definition and the values
+ */
 export type ControlState = {
   params: {
     definition: FxParamDefinitions | null
@@ -31,23 +44,40 @@ export type ControlState = {
   }
 }
 
+/**
+ * The actual runtime state of a project that determines
+ * what the project will display
+ */
 export type RuntimeState = Omit<ProjectState, "cid" | "snippetVersion"> & {
   params: FxParamsData
 }
 
+/**
+ * The definition of the runtime
+ */
 export type RuntimeDefinition = {
   params: FxParamDefinitions | null
   version: string | null
   features: RawTokenFeatures | null
 }
 
+/**
+ * The whole state of the runtime consists of the runtime state
+ * and the runtime definition
+ */
 export type RuntimeWholeState = {
   state: RuntimeState
   definition: RuntimeDefinition
 }
 
+/**
+ * Generic update function for a state
+ */
 export type TUpdateStateFn<T, R> = (data: DeepPartial<T>) => R
 
+/**
+ * Generic state that can be updated
+ */
 export type TUpdateableState<T, R> = T & {
   update: TUpdateStateFn<T, R>
 }
