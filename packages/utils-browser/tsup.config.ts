19f45065c8ab63a895e9ab15a74be42ee7caa500
Léo Pradel <pradel.leo@gmail.com>
2025-01-14 16:21:18 +0100

    [repo] feat: enable next turbo mode in dev

--- packages/utils-browser/tsup.config.ts
+++ packages/utils-browser/tsup.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig, Options } from "tsup"
 
 export default defineConfig((options: Options) => ({
-  entry: ["src/**/*.{ts,tsx}"],
+  entry: ["src/index.ts"],
   outDir: "dist",
   format: ["cjs", "esm"],
-  splitting: true,
   sourcemap: true,
   clean: !options.watch,
   dts: true,
-  bundle: true,
 }))
