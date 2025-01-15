bfae5f43a7b0165d7ee15facc166e792332e22cb
maerzhase <markus@lili16.de>
2025-01-15 13:44:44 +0100

    reduce fxhash/libarries bundle size
    - async export library version content
    - remove cjs build

--- packages/libraries/tsup.config.ts
+++ packages/libraries/tsup.config.ts
@@ -1,15 +1,29 @@
 import { defineConfig, Options } from "tsup"
+import { readFileSync } from "fs"
 
 export default defineConfig((options: Options) => ({
   entry: ["src/index.ts"],
   outDir: "dist",
-  format: ["cjs", "esm"],
+  format: "esm",
   splitting: true,
+  treeshake: true,
   sourcemap: false,
   clean: !options.watch,
   dts: true,
   bundle: true,
-  loader: {
-    ".lib": "text",
-  },
+  esbuildPlugins: [
+    {
+      name: "text-loader",
+      setup(build) {
+        // Handle .txt files
+        build.onLoad({ filter: /\.lib$/ }, async args => {
+          const text = readFileSync(args.path, "utf8")
+          return {
+            contents: `export default ${JSON.stringify(text)}`,
+            loader: "js",
+          }
+        })
+      },
+    },
+  ],
 }))
