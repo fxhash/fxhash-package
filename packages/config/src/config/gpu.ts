07de3723ed0dd370ccea5f1ac5402011ea36ab24
Louis Holley <louisholley@me.com>
2025-01-29 17:17:28 +0000

    update gpuRenderingConfigProd

--- packages/config/src/config/gpu.ts
+++ packages/config/src/config/gpu.ts
@@ -47,6 +47,6 @@ export const gpuRenderingConfigDev: GPURenderingConfig = createEnvConfig({
 })
 
 export const gpuRenderingConfigProd: GPURenderingConfig = createEnvConfig({
-  v111: "arn:aws:batch:us-east-1:759392670381:job-definition/fxhash-batch-prd-fxhash-extract",
-  v132: "arn:aws:batch:...",
+  v111: "arn:aws:batch:us-east-1:759392670381:job-definition/fxhash-batch-prd-fxhash-extract-chrome-v111",
+  v132: "arn:aws:batch:us-east-1:759392670381:job-definition/fxhash-batch-prd-fxhash-extract-chrome-v132",
 })
