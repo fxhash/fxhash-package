6d1b1f5961fc63515921721684860029c6f23eaf
ciphrd <ciphrd@fxhash.xyz>
2025-04-01 16:43:20 +0200

    fix isMounted() for dev

--- packages/client-react/src/hooks/useIsMounted.ts
+++ packages/client-react/src/hooks/useIsMounted.ts
@@ -3,6 +3,8 @@ import { useCallback, useEffect, useRef } from "react"
 export function useIsMounted() {
   const isMounted = useRef(true)
   useEffect(() => {
+    // this line is for dev, because react calls return fn() on dev
+    isMounted.current = true
     return () => {
       isMounted.current = false
     }
