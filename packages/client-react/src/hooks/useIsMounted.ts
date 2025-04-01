35345e8586c95aa4f1dfd0832d6953a7272413cb
ciphrd <ciphrd@fxhash.xyz>
2025-04-01 16:41:36 +0200

    useIsMounted hook

---
+++ packages/client-react/src/hooks/useIsMounted.ts
@@ -0,0 +1,11 @@
+import { useCallback, useEffect, useRef } from "react"
+
+export function useIsMounted() {
+  const isMounted = useRef(true)
+  useEffect(() => {
+    return () => {
+      isMounted.current = false
+    }
+  }, [])
+  return useCallback(() => isMounted.current, [])
+}
