399c4004a5d5d9a5f76bbdfa6c958d54f4ce81ec
maerzhase <markus@lili16.de>
2025-04-16 14:45:18 +0200

    update sdk

--- packages/project-sdk/dist/fxhash.d.ts
+++ packages/project-sdk/dist/fxhash.d.ts
@@ -1,29 +1,46 @@
-import { FxParamDefinition, FxParamType, FxParamValue } from '@fxhash/params/types';
+import { FxParamValue, FxParamType, FxParamProcessors, FxParamDefinition, FxParamsRaw, FxParamsTransformed, FxParamTransformationTypeMap } from '@fxhash/params';
 import { ResettableRandFunction } from '@fxhash/utils';
 
 type FxHashExecutionContext = "standalone" | "capture" | "minting";
-type FxHashApi = {
+type FxhashSdkPrivate = {
+    _version: string;
+    _features?: FxFeatures;
+    _updateParams: (data: FxEmitData) => void;
+    _processors: FxParamProcessors;
+    _params?: FxParamDefinition<FxParamType>[];
+    _rawValues: FxParamsRaw;
+    _paramValues: FxParamsTransformed;
+    _receiveUpdateParams: (data: FxEmitData, onDefault?: () => void) => Promise<void>;
+    _listeners: Record<FxEventId | string, Array<[FxOnEventHandler, FxOnDone?]>>;
+    _propagateEvent: (name: FxEventId, data: any) => Promise<any[][]>;
+    _updateInputBytes: () => void;
+    _emitParams: (data: FxEmitData) => void;
+};
+type _Optional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+interface UserDefinedParams extends _Optional<FxParamDefinition<FxParamType>, "value" | "options"> {
+}
+type FxHashApi = FxhashSdkPrivate & {
     hash: string;
     minter: string;
     iteration: number;
     rand: ResettableRandFunction;
     randminter: ResettableRandFunction;
     context: FxHashExecutionContext;
+    inputBytes?: string;
     preview: () => void;
     isPreview: boolean;
     features: (features: FxFeatures) => void;
-    getFeature: (id: string) => FxFeatureValue | undefined;
+    getFeature: (id: string) => FxFeatureValue;
     getFeatures: () => FxFeatures;
-    stringifyParams: (definitions: FxParamDefinition<FxParamType>[]) => string;
-    params: (paramsDefinitions: FxParamDefinition<FxParamType>[]) => void;
+    stringifyParams: (definitions: FxParamDefinition<FxParamType>[] | Record<string, FxParamValue<FxParamType>>) => string;
+    params: (paramsDefinitions: UserDefinedParams[]) => void;
     getDefinitions: () => FxParamDefinition<FxParamType>[];
-    getParam: (id: string) => FxParamValue<FxParamType>;
-    getParams: () => FxParamValue<FxParamType>;
-    getRawParam: (id: string) => string;
-    getRawParams: () => {
-        string: string;
-    };
-    on: (event: FxEventId, handler: () => void, onDone: () => void) => void;
+    getParam: (id: string) => FxParamTransformationTypeMap[FxParamType];
+    getParams: () => FxParamsTransformed;
+    getRawParam: (id: string) => FxParamValue<FxParamType>;
+    getRawParams: () => Record<string, FxParamValue<FxParamType>>;
+    getRandomParam: (id: string) => FxParamValue<FxParamType>;
+    on: (event: FxEventId, handler: FxOnEventHandler, onDone: FxOnDone) => void;
     emit: (event: FxEventId, data: FxEmitData) => void;
 };
 type FxFeatureValue = string | number | boolean;
@@ -31,6 +48,8 @@ type FxFeatures = Record<string, FxFeatureValue>;
 type FxEventId = "params:update";
 type FxEmitData = Record<string, FxParamValue<FxParamType>>;
 type FxEmitFunction = (event: FxEventId, data: FxEmitData) => void;
+type FxOnEventHandler = (data: FxEmitData) => Promise<boolean> | boolean;
+type FxOnDone = (optInDefault: boolean, newRawValues: FxEmitData) => void;
 interface FxInitOptions {
     params: FxParamDefinition<FxParamType>[];
     features: FxFeatures;
@@ -38,9 +57,10 @@ interface FxInitOptions {
 type SetFeaturesOptions = Pick<FxInitOptions, "features">;
 type SetParamsOptions = Pick<FxInitOptions, "params">;
 declare global {
+    const $fx: typeof window.$fx;
     interface Window {
         $fx: FxHashApi;
     }
 }
 
-export { FxEmitData, FxEmitFunction, FxEventId, FxFeatureValue, FxFeatures, FxHashApi, FxHashExecutionContext, FxInitOptions, SetFeaturesOptions, SetParamsOptions };
+export type { FxEmitData, FxEmitFunction, FxEventId, FxFeatureValue, FxFeatures, FxHashApi, FxHashExecutionContext, FxInitOptions, FxOnDone, FxOnEventHandler, FxhashSdkPrivate, SetFeaturesOptions, SetParamsOptions };
