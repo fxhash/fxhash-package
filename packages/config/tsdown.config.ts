1e7e9cbae4e94240bf18e140b673567a07d99882
Léo Pradel <pradel.leo@gmail.com>
2025-05-14 16:17:36 +0200

    [repo] gql-admin

--- packages/config/tsdown.config.ts
+++ packages/config/tsdown.config.ts
@@ -2,8 +2,6 @@ import { defineConfig, type Options } from "tsdown"
 
 export default defineConfig((options: Options) => ({
   entry: ["src/index.ts"],
-  outDir: "dist",
-  format: "esm",
   sourcemap: true,
   clean: !options.watch,
   dts: true,
