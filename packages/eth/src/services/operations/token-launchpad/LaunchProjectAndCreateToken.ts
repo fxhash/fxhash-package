5b54674e7f6d07bd9c7c19b94bba4ef7b853274a
C-09-07 <c.middlton@gmail.com>
2025-05-15 13:39:54 +0100

    demo section

---
+++ packages/eth/src/services/operations/token-launchpad/LaunchProjectAndCreateToken.ts
@@ -0,0 +1,63 @@
+import { EthereumContractOperation } from "@/services/operations/contractOperation.js"
+import {
+  simulateAndExecuteContract,
+  type SimulateAndExecuteContractRequest,
+} from "@/services/operations/EthCommon.js"
+import { TransactionType } from "@fxhash/shared"
+import { getCurrentChain } from "@/services/Wallet.js"
+import { projectFactoryAbi } from "@/__generated__/wagmi.js"
+import { config } from "@fxhash/config"
+
+export type TTokenLaunchpadLaunchProjectAndCreateTokenEthOperationParams = {
+  // The name of the project
+  name: string
+  // The symbol of the project
+  symbol: string
+  // The initial owner address
+  initialOwner: `0x${string}`
+  // The base URI for the project
+  baseURI: string
+  mintInfo: {
+    price: bigint
+    maxSupply: bigint
+  }
+  tagIds: bigint[]
+  // The amount of FxTokens used to create liquidity
+  purchaseAmount: bigint
+}
+
+export class TokenLaunchpadLaunchProjectAndCreateTokenEthOperation extends EthereumContractOperation<TTokenLaunchpadLaunchProjectAndCreateTokenEthOperationParams> {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function, @typescript-eslint/explicit-function-return-type
+  async prepare() {}
+
+  async call(): Promise<{ type: TransactionType; hash: string }> {
+    const args: SimulateAndExecuteContractRequest<
+      typeof projectFactoryAbi,
+      "launchTokenAndCreateProject"
+    > = {
+      address: config.base.contracts.fx_project_factory,
+      abi: projectFactoryAbi,
+      functionName: "launchTokenAndCreateProject",
+      args: [
+        this.params.name,
+        this.params.symbol,
+        this.params.initialOwner,
+        this.params.baseURI,
+        this.params.mintInfo,
+        this.params.tagIds,
+        this.params.purchaseAmount,
+      ],
+      account: this.manager.address as `0x${string}`,
+      chain: getCurrentChain(this.chain),
+    }
+    const transactionHash = await simulateAndExecuteContract(this.manager, args)
+    return {
+      type: TransactionType.ONCHAIN,
+      hash: transactionHash,
+    }
+  }
+
+  success(): string {
+    return "Successfully created project and token"
+  }
+}
