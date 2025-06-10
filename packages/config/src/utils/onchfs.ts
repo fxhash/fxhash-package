e04647b506db3ca87b6b258376234d6b13fa2cb5
maerzhase <markus@lili16.de>
2025-06-10 09:46:51 +0200

    add onchfs util to config

---
+++ packages/config/src/utils/onchfs.ts
@@ -0,0 +1,10 @@
+export const onchfsRegex: RegExp = new RegExp("^onchfs://")
+
+/**
+ * Returns the CID or a resource, whether it's already a CID or an onchfs://<CID> resource
+ * @param resource either a CID or a onchfs://<CID> string
+ */
+export function onchfsCidFromUriOrCid(resource: string): string {
+  if (!onchfsRegex.test(resource)) return resource
+  return resource.slice(7)
+}
