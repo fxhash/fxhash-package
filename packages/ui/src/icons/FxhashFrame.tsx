71bf3cb677e6e0a88df7ea0db69e37f6298088f5
maerzhase <markus@lili16.de>
2025-08-19 11:08:55 +0200

    add icons

---
+++ packages/ui/src/icons/FxhashFrame.tsx
@@ -0,0 +1,19 @@
+
+import { SVGProps } from "react"
+
+export function IconFxhashFrame(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 350 350"
+      width={16}
+      height={16}
+      {...props}
+    >
+      <g fill="currentColor">
+        <polygon points="200 125 150 125 150 150 125 150 125 200 150 200 150 225 200 225 200 200 225 200 225 150 200 150 200 125" />
+        <path d="M314.85,0H35.15C15.74,0,0,15.74,0,35.15v279.7c0,19.41,15.74,35.15,35.15,35.15h279.7c19.41,0,35.15-15.74,35.15-35.15V35.15c0-19.41-15.74-35.15-35.15-35.15ZM300,150h-25v50h25v100h-100v-25h-50v25H50v-100h25v-50h-25V50h100v25h50v-25h100v100Z" />
+      </g>
+    </svg>
+  )
+}
